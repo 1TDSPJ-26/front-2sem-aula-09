cf801c1d7c28204fab43165448ca9a9ffb4825b8
sophRmd <sophia.ramada02@gmail.com>
2026-09-10 10:48:56 -0300

    feat: Componentes de rotas criados. EditarProduto, Produto, Home e Error.

---
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -0,0 +1,10 @@
+export default function EditarProdutos() {
+  return (
+    <main>
+        <h2>
+            Editar Produto
+        </h2>
+        
+    </main>
+  )
+}
---
+++ my-app/src/routes/Error/index.tsx
@@ -0,0 +1,10 @@
+export default function Error() {
+  return (
+    <main>
+        <h2>
+            ERRO
+        </h2>
+        
+    </main>
+  )
+}
