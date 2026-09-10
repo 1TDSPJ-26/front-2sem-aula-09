ed22b483831b14162efd5a2bbf6a4aa19d43e8f5
joaovjbernardo <jjvictorbernardo@gmail.com>
2026-09-10 12:14:56 -0300

    feat

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,6 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+
+import Home from './routes/Home'
+import Produtos from './routes/Produtos'
+import EditarProdutos from './routes/EditarProdutos'
+import Error from './routes/Error'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/produtos', element: <Produtos /> },
+      { path: '/editar-produtos', element: <EditarProdutos /> }
+    ]
+  }
+])
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
