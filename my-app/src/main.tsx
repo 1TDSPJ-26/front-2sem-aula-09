ef79644307ad2daa5d5755145f23f2861c10bf4e
TailyniDev <tailyni80@gmail.com>
2026-09-10 11:06:17 -0300

    feat : Componentes de rotas criados. EditarProdutos, Produtos, Home e Error.

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,8 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import EditarProdutos from './routes/Produtos/index.tsx'
+import Produtos from "./routes/Produtos/index.tsx"
+import Error from "./routes/Error/index.tsx"
+import Home from './routes/Home/index.tsx'
+
+const router = createBrowserRouter([
+  {path:'/', element: <App/>, errorElement: <Error/>, children:[
+    {path: '/', element:<Home/>},
+    {path: '/produtos', element: <Produtos/>},
+    {path: 'editar-produtos', element:<EditarProdutos/>}
+  ]}
+])
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
