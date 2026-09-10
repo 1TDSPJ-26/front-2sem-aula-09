93c435a699e5dee76299df8b52452c32da102a7f
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-09-10 11:35:44 -0300

    feat : Destructuring
    Signed-off-by: alecarlosjesus <profalexandre.jesus@fiap.com.br>

--- my-app/src/components/Menu/index.tsx
+++ my-app/src/components/Menu/index.tsx
@@ -6,7 +6,6 @@ export default function Menu() {
       <ul>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/produtos'>Produtos</Link></li>
-        <li><Link to='/editar-produtos'>Editar Produtos</Link></li>
       </ul>
     </nav>
   );
