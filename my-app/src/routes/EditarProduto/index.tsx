48e3c173685da1fb4d21ad919ab5d6cc085e7346
leite-1309 <leite.lucas1309@gmail.com>
2026-09-10 12:15:40 -0300

    feat: Recuperação de dados no editar-produtos, utilizando hooks, useEffec, useState e useParams.

--- my-app/src/routes/EditarProduto/index.tsx
+++ my-app/src/routes/EditarProduto/index.tsx
@@ -1,3 +1,41 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router"
+
+type TipoProduto = {
+    id: number;
+    nome: string;
+    preco: number
+}
+
+const listaProdutos:TipoProduto[] = [
+    { id: 1, nome: "Produto 1", preco: 10.0 },
+    { id: 2, nome: "Produto 2", preco: 20.0 },
+    { id: 3, nome: "Produto 3", preco: 30.0 },
+];
+
 export default function EditarProduto(){
-    return <h1>Editar produtos</h1>
+    
+    const { id } = useParams<string>();
+
+    const[produto, setProduto] = useState<TipoProduto>({} as TipoProduto);
+
+    useEffect(()=>{
+        const prodEncontrado = listaProdutos.find( (p)=> p.id === Number(id) );
+        
+        setProduto(prodEncontrado!);
+
+    },[])
+
+    return <main>
+        <h1>Editar produtos</h1>
+
+        {produto?(
+            <div>
+                <p>Nome do produto: {produto.nome}</p>
+                <p>Preço do produto: {produto.preco}</p>
+            </div>) : (
+                <p>Produto não encontrado</p>)
+                }
+    
+    </main>
 }
